d582884953863b7c20197f7928f30b2c5c3a1917
trainee113 <132211395+trainee113@users.noreply.github.com>
2023-04-30 08:34:08 -0600

    Ticket.php
    ssssss

--- ticket.php
+++ ticket.php
@@ -10,32 +10,32 @@
     # Encabezado y datos de la empresa #
     $pdf->SetFont('Arial','B',10);
     $pdf->SetTextColor(0,0,0);
-    $pdf->MultiCell(0,5,utf8_decode(strtoupper("Nombre de empresa")),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode(strtoupper("Comida Rapida "El Mirador" ")),0,'C',false);
     $pdf->SetFont('Arial','',9);
     $pdf->MultiCell(0,5,utf8_decode("RUC: 0000000000"),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Direccion San Salvador, El Salvador"),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Teléfono: 00000000"),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Email: correo@ejemplo.com"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Direccion Calle Jacarandas 103 Plaza Forum Cuernavaca"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Teléfono: 5590901816"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Email: atn.cliente.elmirador.mx"),0,'C',false);
 
     $pdf->Ln(1);
     $pdf->Cell(0,5,utf8_decode("------------------------------------------------------"),0,0,'C');
     $pdf->Ln(5);
 
-    $pdf->MultiCell(0,5,utf8_decode("Fecha: ".date("d/m/Y", strtotime("13-09-2022"))." ".date("h:s A")),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Caja Nro: 1"),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Cajero: Carlos Alfaro"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Fecha: ".date("d/m/Y", strtotime("30-04-2023"))." ".date("h:s A")),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Mesa Nro: 4"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Mesero/a: Linda Orquiza"),0,'C',false);
     $pdf->SetFont('Arial','B',10);
-    $pdf->MultiCell(0,5,utf8_decode(strtoupper("Ticket Nro: 1")),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode(strtoupper("Ticket Nro: 901")),0,'C',false);
     $pdf->SetFont('Arial','',9);
 
     $pdf->Ln(1);
     $pdf->Cell(0,5,utf8_decode("------------------------------------------------------"),0,0,'C');
     $pdf->Ln(5);
 
-    $pdf->MultiCell(0,5,utf8_decode("Cliente: Carlos Alfaro"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Cliente: Erik Baltazar"),0,'C',false);
     $pdf->MultiCell(0,5,utf8_decode("Documento: DNI 00000000"),0,'C',false);
     $pdf->MultiCell(0,5,utf8_decode("Teléfono: 00000000"),0,'C',false);
-    $pdf->MultiCell(0,5,utf8_decode("Dirección: San Salvador, El Salvador, Centro America"),0,'C',false);
+    $pdf->MultiCell(0,5,utf8_decode("Dirección: Cuernavaca/Morelos"),0,'C',false);
 
     $pdf->Ln(1);
     $pdf->Cell(0,5,utf8_decode("-------------------------------------------------------------------"),0,0,'C');
@@ -54,13 +54,13 @@
 
 
     /*----------  Detalles de la tabla  ----------*/
-    $pdf->MultiCell(0,4,utf8_decode("Nombre de producto a vender"),0,'C',false);
+    $pdf->MultiCell(0,4,utf8_decode("Restaurante1"),0,'C',false);
     $pdf->Cell(10,4,utf8_decode("7"),0,0,'C');
-    $pdf->Cell(19,4,utf8_decode("$10 USD"),0,0,'C');
-    $pdf->Cell(19,4,utf8_decode("$0.00 USD"),0,0,'C');
-    $pdf->Cell(28,4,utf8_decode("$70.00 USD"),0,0,'C');
+    $pdf->Cell(19,4,utf8_decode("$1,000.00 MXN"),0,0,'C');
+    $pdf->Cell(19,4,utf8_decode("$2,400.00 MXN"),0,0,'C');
+    $pdf->Cell(28,4,utf8_decode("$120.00 MXN"),0,0,'C');
     $pdf->Ln(4);
-    $pdf->MultiCell(0,4,utf8_decode("Garantía de fábrica: 2 Meses"),0,'C',false);
+    $pdf->MultiCell(0,4,utf8_decode("Gracias por su estancia"),0,'C',false);
     $pdf->Ln(7);
     /*----------  Fin Detalles de la tabla  ----------*/
 
@@ -73,13 +73,13 @@
     # Impuestos & totales #
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("SUBTOTAL"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("+ $70.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("+ $120.00 MXN"),0,0,'C');
 
     $pdf->Ln(5);
 
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("IVA (13%)"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("+ $0.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("+ $80.00 USD"),0,0,'C');
 
     $pdf->Ln(5);
 
@@ -89,25 +89,25 @@
 
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("TOTAL A PAGAR"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("$70.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("$3,720.00 MXN"),0,0,'C');
 
     $pdf->Ln(5);
     
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("TOTAL PAGADO"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("$100.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("$3,800.00 MXN"),0,0,'C');
 
     $pdf->Ln(5);
 
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("CAMBIO"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("$30.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("$0.00 MXN"),0,0,'C');
 
     $pdf->Ln(5);
 
     $pdf->Cell(18,5,utf8_decode(""),0,0,'C');
     $pdf->Cell(22,5,utf8_decode("USTED AHORRA"),0,0,'C');
-    $pdf->Cell(32,5,utf8_decode("$0.00 USD"),0,0,'C');
+    $pdf->Cell(32,5,utf8_decode("$0.00 MXN"),0,0,'C');
 
     $pdf->Ln(10);
 
